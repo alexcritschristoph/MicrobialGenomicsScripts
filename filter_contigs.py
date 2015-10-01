cf19460d8ddad65b0008cb94cf5601eff9680b7f
Alex Crits-Christoph <acritsc1@jhu.edu>
2015-10-01 01:41:32 -0400

    Update filter_contigs.py
    bugfix

--- filter_contigs.py
+++ filter_contigs.py
@@ -5,7 +5,7 @@ handle = open(sys.argv[1], "rU")
 l = SeqIO.parse(handle, "fasta")
 counter = 0
 for s in l:
-	if len(s.seq) >= sys.argv[2]:
+	if len(s.seq) >= int(sys.argv[2]):
 		print ">" + s.id
 		print s.seq
 		counter += 1
