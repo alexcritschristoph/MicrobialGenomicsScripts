652f64d8a65b06ec65fa76cc0fce766eaec7bec6
Alex Crits-Christoph <acritsc1@jhu.edu>
2016-09-20 15:41:57 -0700

    fixed

--- top_10_contigs.py
+++ top_10_contigs.py
@@ -8,13 +8,13 @@ l = SeqIO.parse(handle, "fasta")
 counter = 0
 contig_lengths = {}
 contigs = {}
+c = []
 for s in l:
     if len(str(s.seq)) > 5000:
 		     contigs[s.id] = s.seq
 		     contig_lengths[s.id] = len(str(s.seq))
-
-sorted_x = sorted(contig_lengths.items(), key=operator.itemgetter(0))
-sorted_x.reverse()
+                     c.append(len(s.seq))
+sorted_x = sorted(contig_lengths.items(), key=lambda x:x[1], reverse=True)
 
 for i in range(0,10):
      print str(sorted_x[i][0]) + ":" + str(sorted_x[i][1])
