5660b0da9f07813bbea58a0f0e1e7aa125fad4df
Alex Crits-Christoph <acritsc1@jhu.edu>
2018-06-15 17:48:07 -0700

    Update calc_coverage_breadth.py

--- calc_coverage_breadth.py
+++ calc_coverage_breadth.py
@@ -4,6 +4,7 @@ import sys
 
 gene_list = sys.argv[1]
 bam = sys.argv[2]
+read_length = sys.argv[3]
 
 f = open(gene_list)
 positions = []
@@ -21,14 +22,10 @@ for pos in positions:
 
 	b = 0 
 	b2 = 0
-	cov = []
 	for pileupcolumn in samfile.pileup(pos[0], pos[1], pos[2], stepper = 'nofilter'):
 
 		if pileupcolumn.reference_pos >= pos[1] and pileupcolumn.reference_pos < pos[2]:
-			cov.append(pileupcolumn.nsegments)
 			b2 += 1
 			if pileupcolumn.nsegments > 0:
 				b += 1
-
-	coverage = round(float(sum(cov)) / float(len(cov)),3)
-	print(pos[0] + "\t" + str(pos[1]) + "\t" + str(pos[2]) + "\t" + str(i) + "\t" + str(coverage) + "\t" + str(float(b) / (pos[2]-pos[1])))
+print(pos[0] + "\t" + str(pos[1]) + "\t" + str(pos[2]) + "\t" + str(i) + "\t" + str(float(i)*int(read_length) / (pos[2]-pos[1])) + "\t" + str(float(b) / (pos[2]-pos[1])))
